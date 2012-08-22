844f876c76295fae1ec773d28ce6be8184142797
Dearth <pjcashin42@gmail.com>
2012-08-22 17:57:52 -0400

    Added test.go and test.

--- Template/src/gui/test.go
+++ Template/src/gui/test.go
@@ -6,10 +6,27 @@ import(
 )
 
 func main () {
-	
+
 	gtk.Init(&os.Args)
 	window := gtk.Window(gtk.GTK_WINDOW_TOPLEVEL)
 
+	window.Connect("delete-event", func(){
+		println("Delete-event occured\n")
+		})
+
+	window.Connect("destroy", func(){ gtk.MainQuit()})
+
+	button := gtk.ButtonWithLabel("Hello, World")
+
+	button.Clicked(func() {
+		println("Hello, World\n")
+	})
+
+	button.Clicked(func() {
+		gtk.MainQuit()
+	})
+
+	window.Add(button)
 	window.ShowAll()
 	gtk.Main()
 }
